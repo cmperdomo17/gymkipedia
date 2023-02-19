23d158831969e3209727124eeaa60eded0f3f55d
Carlos Mario Perdomo Ramos <122324408+cmperdomo17@users.noreply.github.com>
2023-02-18 19:18:22 -0500

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+const hamburgerBtn = document.querySelector('.hamburguer');
+const navigationMenu = document.querySelector('.menu-navegacion');
+
+console.log(navigationMenu);
+console.log(hamburgerBtn);
+
+hamburgerBtn.addEventListener('click', () => {
+    alert("Haz hecho clic en el botón hamburguesa");
+});
+
+
